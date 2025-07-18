3c61975d08371234fa6ab7d304ac9688a5afccf8
00MATRX <geojonbailey@gmail.com>
2025-07-18 10:40:31 -0400

    Delete telegram_x_bot directory

--- telegram_x_bot/config.py
+++
@@ -1,9 +0,0 @@
-# X API credentials (from developer.x.com/portal > Keys and Tokens)
-CONSUMER_KEY = "YOUR_CONSUMER_KEY"
-CONSUMER_SECRET = "YOUR_CONSUMER_SECRET"
-ACCESS_TOKEN = "YOUR_ACCESS_TOKEN"
-ACCESS_TOKEN_SECRET = "YOUR_ACCESS_SECRET"
-
-# Telegram credentials
-TELEGRAM_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"  # From @BotFather
-ALLOWED_USER_ID = 0  # int, e.g., 123456789 (from @userinfobot)
